5dcc8783c327d56e26cd432583e1ba904268ed51
arkadii.vodolazskyi <arkadii.vodolazskyi@gmail.com>
2023-11-27 23:30:25 +0200

    Coded TransactionsList.

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,9 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        {
+          id: 1,
+          text: 'Flowers',
+          amount: -20
+        },
+        {
+          id: 2,
+          text: 'Book',
+          amount: -20
+        },
+        {
+          id: 3,
+          text: 'Salary',
+          amount: 200
+        },
+      ]
+    }
+  }
+}
+</script>
+
 <template>
   <div>
     <h2>Transactions</h2>
     <ul>
-      <li>
-        Transaction
+      <li
+        v-for="t in transactions"
+        :key="t.id"
+        :class="{
+          income: t.amount > 0,
+          expense: t.amount < 0
+        }"
+      >
+        <span>{{ t.text }}</span>
+        <span>{{ t.amount }}</span>
+        <button>Delete</button>
       </li>
     </ul>
   </div>
